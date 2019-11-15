fcf8b4108c7d6a0616e27f0c37931857adf96394
sramyasatya <57611703+sramyasatya@users.noreply.github.com>
2019-11-15 10:38:33 +0530

    java programs

--- Dice/src/arithmeticmainprog/Arithmeticmain.java
+++ Dice/src/arithmeticmainprog/Arithmeticmain.java
@@ -1,5 +1,36 @@
 package arithmeticmainprog;
-import 
+import java.util.Scanner;
 public class Arithmeticmain {
-
+	public static void main(String args[])
+	{
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	int c=sc.nextInt();
+	
+	if(a+b==c)
+	{
+		System.out.println("Add");
+	}
+	if(a-b==c)
+	{
+		System.out.println("Subtract");
+	}	
+	if(a*b==c)
+	{
+		System.out.println("multiply");
+	}
+	if(a/b==c)
+	{
+		System.out.println("Divide");
+	}
+	if(a%b==c)
+	{
+		System.out.println("Modulo");
+	}
+	else
+	{
+		System.out.println("Not Possible");
+	}
+	}
 }
--- Dice/src/queue/Queuemain.java
+++ Dice/src/queue/Queuemain.java
@@ -1,6 +1,72 @@
 package queue;
 import java.util.Scanner;
-public class Queuemain {
-Scanner sc=new Scanner(System.in);
 
+import java.util.*;
+
+class Student implements Comparable<Student>{
+    private int token;
+    private String fname;
+    private double cgpa;
+    
+    public int compareTo(Student st) {
+        if (getCgpa() != st.getCgpa()) { 
+            return getCgpa() > st.getCgpa() ? -1 : 1; 
+        } 
+        int val = getFname().compareTo(st.getFname()); 
+        if (val != 0) { 
+            return val; 
+        } 
+        return getToken() < st.getToken() ? -1 : 1; 
+    }
+    
+    
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.token = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }   
+    public int getToken() {
+    	return token;
+    	}
+    public String getFname() {
+    	return fname;
+    	}
+    public double getCgpa() {
+    	return cgpa;
+    	}
 }
+public class Queuemain {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int totalEvents = Integer.parseInt(in.nextLine());
+        PriorityQueue<Student> queue = new PriorityQueue<Student>();
+
+        while(totalEvents>0){
+            String event = in.next();
+            if (event.equals("ENTER")){
+                String name = in.next();
+                double cgpa = in.nextDouble();
+                int token = in.nextInt();
+                queue.add(new Student(token, name, cgpa));
+            }
+            else if(event.equals("SERVED")){
+                if (queue.size()>0){
+                    queue.poll();
+                }
+            }
+            totalEvents--;
+        }
+        
+        if (queue.size() > 0) {
+            Student st = null;
+            while ((st = queue.poll()) != null) {
+                System.out.println(st.getFname()); 
+            }
+        } else {
+            System.out.println("EMPTY"); 
+        }
+    }
+	}
+
