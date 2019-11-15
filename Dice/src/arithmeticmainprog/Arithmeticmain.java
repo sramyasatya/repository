572686c77c14cb6e3fb2fe4c131cb4e0c50f7684
sramyasatya <57611703+sramyasatya@users.noreply.github.com>
2019-11-15 10:56:15 +0530

    javapgm

--- Dice/src/arithmeticmainprog/Arithmeticmain.java
+++ Dice/src/arithmeticmainprog/Arithmeticmain.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Arithmeticmain {
 	public static void main(String args[])
 	{
+		int count=0;
 	Scanner sc=new Scanner(System.in);
 	int a=sc.nextInt();
 	int b=sc.nextInt();
@@ -11,24 +12,29 @@ public class Arithmeticmain {
 	if(a+b==c)
 	{
 		System.out.println("Add");
+		count++;
 	}
 	if(a-b==c)
 	{
 		System.out.println("Subtract");
+		count++;
 	}	
 	if(a*b==c)
 	{
 		System.out.println("multiply");
+		count++;
 	}
 	if(a/b==c)
 	{
 		System.out.println("Divide");
+		count++;
 	}
 	if(a%b==c)
 	{
 		System.out.println("Modulo");
+		count++;
 	}
-	else
+	else if(count==0)
 	{
 		System.out.println("Not Possible");
 	}
